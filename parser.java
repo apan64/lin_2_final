bc760d61116b4c4ecd8a11ff171586d7334a74f4
davpapp <david.papp@students.olin.edu>
2016-12-14 18:27:13 -0500

    Update parser.java

--- parser.java
+++ parser.java
@@ -33,91 +33,24 @@ public class NKModel {
 		}
 		
 		int nmin = 1;
-		int nmax = 50;
+		int nmax = 10;
 		int mmin = 10;
-		int mmax = 100;
+		int mmax = 20;
 		smaLandscape Land = new smaLandscape(nmin, nmax, mmin, mmax);
 		double[][] landscape = new double[nmax - nmin][mmax - mmin];
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 1; i++) {
 			landscape = Land.step(data);
+			display(landscape);
 		}
-	
 	}
 	
-	public static double calcSMA(ArrayList<dayData> data, int n1, int n2) {
-		// n1 is the number of days the short term SMA is calculated from 
-		// n2 is the number of days the long term EMA is calculated from
-		
-		double short_term_SMA_sum = 0.0;
-		double long_term_SMA_sum = 0.0;
-		double short_term_SMA = 0.0;
-		double long_term_SMA = 0.0;
-		
-		double profit = 0.0;
-		double last_sell_value = 0.0;
-		boolean long_term_greater = false;
-		
-		for (int i = 0; i < data.size(); i++) {
-			short_term_SMA_sum += data.get(i).close;
-			long_term_SMA_sum += data.get(i).close;
-			
-			if (i >= n1) {
-				short_term_SMA_sum -= data.get(i - n1).close;
-				short_term_SMA = short_term_SMA_sum / n1;
+	public static void display(double[][] landscape) {
+		for (int i = 0; i < landscape.length; i++) {
+			for (int j = 0; j < landscape[0].length; j++) {
+				System.out.print(landscape[i][j] + ", ");
 			}
-			if (i >= n2) {
-				long_term_SMA_sum -= data.get(i - n2).close;
-				long_term_SMA = long_term_SMA_sum / n2;
-				
-				if (short_term_SMA > long_term_SMA && long_term_greater) {
-					profit += (data.get(i).close);
-					last_sell_value = profit;
-					long_term_greater = false;
-					//System.out.println("SELL on " + data.get(i).date);
-				}
-				if (long_term_SMA > short_term_SMA && !long_term_greater) {
-					profit -= data.get(i).close;
-					long_term_greater = true;
-					//System.out.println("BUY on " + data.get(i).date);
-				}
-			}
-			//System.out.println("Short term: " + short_term_SMA + ", Long term: " + long_term_SMA);
+			System.out.println();
 		}
-		return last_sell_value;
 	}
 	
-	public static double calcEMA(ArrayList<dayData> data, int n1, int n2, double alpha) {
-		double long_term_SMA_sum = 0.0;
-		double short_term_EMA = 0.0;
-		double long_term_SMA = 0.0;
-		
-		double profit = 0.0;
-		double last_sell_value = 0.0;
-		boolean long_term_greater = false;
-		
-		for (int i = 0; i < data.size(); i++) {
-			short_term_EMA = short_term_EMA + alpha * (data.get(i).close - short_term_EMA);
-			long_term_SMA_sum += data.get(i).close;
-			
-			if (i >= n2) {
-				long_term_SMA_sum -= data.get(i - n2).close;
-				long_term_SMA = long_term_SMA_sum / n2;
-				
-				if (short_term_EMA > long_term_SMA && long_term_greater) {
-					profit += (data.get(i).close);
-					last_sell_value = profit;
-					long_term_greater = false;
-					//System.out.println("SELL on " + data.get(i).date);
-				}
-				if (long_term_SMA > short_term_EMA && !long_term_greater) {
-					profit -= data.get(i).close;
-					long_term_greater = true;
-					//System.out.println("BUY on " + data.get(i).date);
-				}
-			}
-			//System.out.println("Short term: " + short_term_EMA + ", Long term: " + long_term_SMA);
-		}
-		return last_sell_value;
-	}
-
 }
