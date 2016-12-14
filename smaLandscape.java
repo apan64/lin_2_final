8f4484b0d503b7f58a31d0ff8c7e0bfae8308e6f
davpapp <david.papp@students.olin.edu>
2016-12-14 18:54:39 -0500

    final

--- smaLandscape.java
+++ smaLandscape.java
@@ -13,6 +13,7 @@ public class smaLandscape
    public int agentCount;
    public int idCount;
    public Random rnd = new Random();
+   public double global_max;
    
     public smaLandscape(int min1, int max1, int min2, int max2)
     {
@@ -20,6 +21,7 @@ public class smaLandscape
         param_2_min = min2;
         param_1_max = max1;
         param_2_max = max2;
+        global_max = -1000;
         landscape = new double[max1 - min1 + 1][max2 - min2 + 1];
         agents = new smaAgent[10];
         for(int i = 0; i < 10; i++){
@@ -28,8 +30,7 @@ public class smaLandscape
         }
         for(int i = 0; i <= (max1 - min1); i++){
             for(int j = 0; j <= (max2 - min2); j++){
-                //int randInt = rnd.nextInt(10)
-                landscape[i][j] = 0;
+                landscape[i][j] = -1000;
             }
         }
     }
@@ -37,8 +38,15 @@ public class smaLandscape
 
     public double fitness(int i, int j, ArrayList<dayData> data)
     {
-    	System.out.println(calcSMA(data, i, j));
-    	return calcSMA(data, i, j);
+    	if (i == 0 || j == 0) {
+    		System.out.println("BOUNDARY----------------------------");
+    		return -1000.0;
+    	}
+    	double fit = calcSMA(data, i, j);
+    	if (fit != 0) {
+    		global_max = Math.max(global_max, fit);
+    	}
+    	return fit;
         
         // return calcEMA(data, i, j, 0.05);
     }
@@ -49,43 +57,55 @@ public class smaLandscape
             landscape[a.i][a.j] = curFit;
             double[] fitnesses = new double[4];
             if(a.i > 0){
-                if(landscape[a.i - 1][a.j] == 0){
+                if(landscape[a.i - 1][a.j] == -1000){
                     fitnesses[0] = fitness(a.i - 1, a.j, data);
                 }
                 else{
                     fitnesses[0] = landscape[a.i - 1][a.j];
                 }
             }
+            else {
+            	fitnesses[0] = -1000;
+            }
             if(a.i < param_1_max - param_1_min){
-                if(landscape[a.i + 1][a.j] == 0){
+                if(landscape[a.i + 1][a.j] == -1000){
                     fitnesses[1] = fitness(a.i + 1, a.j, data);
                 }
                 else{
                     fitnesses[1] = landscape[a.i + 1][a.j];
                 }
             }
+            else {
+            	fitnesses[1] = -1000;
+            }
             if(a.j > 0){
-                if(landscape[a.i][a.j - 1] == 0){
+                if(landscape[a.i][a.j - 1] == -1000){
                     fitnesses[2] = fitness(a.i, a.j - 1, data);
                 }
                 else{
                     fitnesses[2] = landscape[a.i][a.j - 1];
                 }
             }
+            else {
+            	fitnesses[2] = -1000;
+            }
             if(a.j < param_2_max - param_2_min){
-                if(landscape[a.i][a.j + 1] == 0){
+                if(landscape[a.i][a.j + 1] == -1000){
                     fitnesses[3] = fitness(a.i, a.j + 1, data);
                 }
                 else{
                     fitnesses[3] = landscape[a.i][a.j + 1];
                 }
             }
+            else {
+            	fitnesses[3] = -1000;
+            }
             int maxPos = 0;
-            for(int i = 1; i < 4; i++){
-                if(fitnesses[maxPos] == 0 && fitnesses[i] != 0){
+            for(int i = 0; i < 4; i++){
+                if(fitnesses[maxPos] == -1000 && fitnesses[i] != -1000){
                     maxPos = i;
                 }
-                if(fitnesses[i] != 0 && fitnesses[i] > fitnesses[maxPos]){
+                if(fitnesses[i] != -1000 && fitnesses[i] > fitnesses[maxPos]){
                     maxPos = i;
                 }
             }
